0c3072f88c2dd806d5ea1fc4281b4e1005b3a1eb
sunding <mail.qq.sunding@qq.com>
2021-03-09 22:54:54 +0800

    first

---
+++ host/jsx/main.jsx
@@ -0,0 +1,8 @@
+﻿var dodo = function (info)
+{
+    // document.write(txt.length)
+    // app.documents.add();
+    app.setSDKEventMessage("Sunding", "info");
+    // alert("NoNoNONONONONONON");
+    // console.log("DDDDD");
+}
